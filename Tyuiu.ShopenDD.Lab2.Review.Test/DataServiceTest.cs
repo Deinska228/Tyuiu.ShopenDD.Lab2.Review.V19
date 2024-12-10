130ac145dd6ece2405a11de5490adbbcc27d0471
Denis Shopen <shopenden@gmail.com>
2024-12-11 00:15:01 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.ShopenDD.Lab2.Review.Test/DataServiceTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ShopenDD.Lab2.Review.V19.Lib;
+
+namespace Tyuiu.ShopenDD.Lab2.Review.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMassFunction()
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+
+            int len = stopValue - startValue + 1;
+            double[] valueWaitArray = new double[len];
+
+            valueWaitArray[0] = 25.86;
+            valueWaitArray[1] = 16.79;
+            valueWaitArray[2] = 9.66;
+            valueWaitArray[3] = 4.44;
+            valueWaitArray[4] = 1.38;
+            valueWaitArray[5] = 0;
+            valueWaitArray[6] = 1.38;
+            valueWaitArray[7] = 4.44;
+            valueWaitArray[8] = 9.66;
+            valueWaitArray[9] = 16.79;
+            valueWaitArray[10] = 25.86;
+
+
+            double[] res = new double[len];
+            res = ds.GetMassFunction(startValue, stopValue);
+            CollectionAssert.AreEqual(valueWaitArray, res);
+        }
+    }
+}
---
+++ Tyuiu.ShopenDD.Lab2.Review.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShopenDD.Lab2.Review.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShopenDD.Lab2.Review.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("25ddd0be-c3d5-4aef-b8ab-2f9ce83a39ec")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
